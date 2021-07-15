0de0bf7c9e22ffe1c0b3864116b8c891292886a7
Robert Smith <67526754+therls@users.noreply.github.com>
2021-07-14 18:35:32 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "personal-portfolio",
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"],
+};
