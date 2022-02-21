3f1042025a7afd3e0736aa5b71f3c0f7a5a274e0
Robert Smith <67526754+therls@users.noreply.github.com>
2022-02-21 02:45:44 -0800

    First Public push

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
+  pathPrefix: "/portfolio-website",
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.codewithsmith.com",
     title: "personal-portfolio",
   },
   plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"],
